5324fb0d0ab2816fa81eef3cd49d413825220044
llins <lucas.lins@surittec.com.br>
2015-05-06 10:56:24 -0300

    Refatoração SURI-CDI -> SURI-FACES

---
+++ src/main/java/br/com/surittec/surifaces/converter/CepConverter.java
@@ -0,0 +1,49 @@
+/*
+ * SURITTEC
+ * Copyright 2015, SURITTEC CONSULTORIA LTDA, 
+ * and individual contributors as indicated by the @authors tag
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+package br.com.surittec.surifaces.converter;
+
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.convert.Converter;
+import javax.faces.convert.FacesConverter;
+
+import br.com.surittec.util.data.CepUtil;
+
+/**
+ * Conversor de CEP
+ * 
+ * @author Lucas Lins
+ *
+ */
+@FacesConverter("br.com.surittec.surifaces.converter.CepConverter")
+public class CepConverter implements Converter {
+
+	@Override
+	public Object getAsObject(FacesContext context, UIComponent component, String value) {
+		return CepUtil.unformat(value);
+	}
+
+	@Override
+	public String getAsString(FacesContext context, UIComponent component, Object value) {
+		return CepUtil.format((String) value);
+	}
+
+}
